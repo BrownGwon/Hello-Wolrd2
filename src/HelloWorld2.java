1adfc3537dbad2f57489536fbe4e856cc0366509
Administrator <Administrator@MSDN-SPECIAL>
2019-01-13 22:45:48 +0900

    배열과 이중 for문 을 이용한 구구단

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,49 +1,103 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class HelloWorld2 {
 	public static void main(String[] args){
 	
-		
-			    System.out.println("구구단 중 출력할 단은? : ");
-				Scanner scanner = new Scanner(System.in); //문자 입력을 명령을 쓴다고 컴퓨터에게 알려줌
+			
 				
-				
-				int number = scanner.nextInt(); //scanner.nextInt(); <- 변수를 입력값으로 할당시켜주는것
-				
-				
-				System.out.println("사용자가 입력한 값 : " + number);
-		
-				
-				if (number < 2){
-					System.out.println("입력값은 2~9까지 입력을 하셔야 됩니다.");
-					System.out.println("구구단 중 출력할 단은? : ");
-				
-					System.out.println("사용자가 입력한 값 : " + number);
-				}else if (number > 9){
-					System.out.println("입력값은 2~9까지 입력을 하셔야 됩니다.");
-					System.out.println("구구단 중 출력할 단은? : ");
-				
-					System.out.println("사용자가 입력한 값 : " + number);
+				int[] result = new int[10];
+				int[] results = new int[9];
+				for(int i = 2; i < result.length; i++){
+					System.out.println("============     ");	
+					System.out.println(i + "단");
+					System.out.println("============     ");
+					for(int j = 0; j < results.length; j++){
+						results[j] = i * (j + 1);	//result[i] i만 붙혀주면됨
+						System.out.println(results[j]);
+					}
 					
-					
-				}else if (number < 9){
-				
-						
-					for(int i = 1; i < 11;  i++){
-						System.out.println(number * i);
-				
-						if(i == 9) {
-							i = 0;
-							number ++;
-							if(number == 10)
-								break;
-							System.out.println("================ 새로운단수가 추가 되었습니다. " + number);	
-						}
-
-							
 				}
+				
+				                  
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+								//String[] classGroup = {"최진혁","최유빈","한아람","이고잉","박가람"};//배열 선언 1번 방법 문자열로 이루어진 배열 
 								
-				}
+		
+//								String[] classGroup = new String[4];//배열 선언 2번 방법 String 앞에 new 는 아직 객체지향을 안배웠기때문에 패스 
+//								classGroup[0] = "최진혁"; // 위에서 크기를 만들어주고 classGroup[0] 에 최진혁 데이터를 넣어줌
+//								classGroup[1] = "도도도";
+//								classGroup[2] = "사사사";
+//								classGroup[3] = "미미미";
+//								System.out.println(classGroup[0]);// 항상 배열은 0번 부터시작
+//								System.out.println(classGroup[1]);// 대괄호[1] 안에 있는 숫자는 인덱스 이다 
+//								System.out.println(classGroup[2]);
+//								System.out.println(classGroup[3]);
+//								System.out.println(classGroup.length);	//배열의 총 갯수 즉 크기 
+//		
+		
+		
+		
+		
+//			    System.out.println("구구단 중 출력할 단은? : ");
+//				Scanner scanner = new Scanner(System.in); //문자 입력을 명령을 쓴다고 컴퓨터에게 알려줌
+//				
+//				
+//				int number = scanner.nextInt(); //scanner.nextInt(); <- 변수를 입력값으로 할당시켜주는것
+//				
+//				
+//				System.out.println("사용자가 입력한 값 : " + number);
+//		
+//				
+//				if (number < 2){
+//					System.out.println("입력값은 2~9까지 입력을 하셔야 됩니다.");
+//					System.out.println("구구단 중 출력할 단은? : ");
+//				
+//					System.out.println("사용자가 입력한 값 : " + number);
+//				}else if (number > 9){
+//					System.out.println("입력값은 2~9까지 입력을 하셔야 됩니다.");
+//					System.out.println("구구단 중 출력할 단은? : ");
+//				
+//					System.out.println("사용자가 입력한 값 : " + number);
+//					
+//					
+//				}else if (number < 9){
+//				
+//						
+//					for(int i = 1; i < 11;  i++){
+//						System.out.println(number * i);
+//				
+//						if(i == 9) {
+//							i = 0;
+//							number ++;
+//							if(number == 10)
+//								break;
+//							System.out.println("================ 새로운단수가 추가 되었습니다. " + number);	
+//						}
+//
+//							
+//				}
+//								
+//				}
 	}
 }
 
